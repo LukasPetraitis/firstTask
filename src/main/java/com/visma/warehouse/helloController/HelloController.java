08a0dc978a8c10f8231508cb2f32cbc328a05e9c
LukasPetraitis <lukaspetraitis90@gmail.com>
2022-04-28 13:26:17 +0300

    fixed package name

--- src/main/java/com/visma/warehouse/helloController/HelloController.java
+++ src/main/java/com/visma/warehouse/helloController/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.firstTask.helloController;
+package com.visma.warehouse.helloController;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/com/visma/warehouse/FirstTaskApplicationTests.java
+++ src/test/java/com/visma/warehouse/FirstTaskApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.firstTask;
+package com.visma.warehouse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
